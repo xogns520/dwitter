c3115972ff52972d22a3eec2bdac6acdf69d79b4
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-06-27 19:59:41 +0900

    Update web.js

--- web.js
+++ web.js
@@ -1,34 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
-app.set('views',"examples/views");
+//app.set('view engine', 'ejs');
+//app.set('views',"examples/views");
 
 // use res.render to load up an ejs view file
 
 // index page 
 app.get('/', function(req, res) {
-	getRank(function(drinks){
-		console.log("in appget",drinks);
-		var tagline = "TBD";
-    		res.render('pages/index', {
-        	drinks: drinks,
-        	tagline: tagline
-    		});
-		
-	});
-	//console.log(drinks);
-	/***
-    var drinks = [
-        { name: 'Bloody Mary', drunkness: 3 },
-        { name: 'Martini', drunkness: 5 },
-        { name: 'Scotch', drunkness: 10 }
-    ];
-    ***/
-
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-
 
 app.listen(process.env.PORT);
