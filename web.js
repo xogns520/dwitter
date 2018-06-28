a090bf82f131fc7265ded25563f322632263a100
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-06-28 19:08:04 +0900

    Update web.js

--- web.js
+++ web.js
@@ -2,9 +2,30 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+//mongo DB
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MONGODB_URI;
+
 // set the view engine to ejs
 //app.set('view engine', 'ejs');
 //app.set('views',"examples/views");
+
+
+//custom functions
+function saveData(account, data){
+	MongoClient.connect(url, function(err, db) {
+   		var dbo = db.db("heroku_dg3d93pq");
+		var tod = Date.now();
+
+   		var myobj = { account : account, data : data, date : tod, voting : 0, payout : 0 };
+   		dbo.collection("board").insertOne(myobj, function(err, res){
+    			if (err) throw err;
+    			//console.log("1 document inserted");
+    			db.close();   
+   		});
+  	}); 
+}
 
 // use res.render to load up an ejs view file
 
@@ -28,6 +49,7 @@ var path = require('path');
 	  var data = req.body.data;
 	  console.log("write event", user, data);
 	  //save this data to mongoDB//
+	  saveData(user, data);
 	  res.send("OK");
   });
 
