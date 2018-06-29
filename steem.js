b58539b09cc71b526321db6cbed4b9d163d93564
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-06-29 21:53:01 +0900

    Create steem.js

---
+++ steem.js
@@ -0,0 +1,101 @@
+var steem = require('steem');
+
+//After writing, this needs cool down time to create the block chain
+function writingReply(child_permlink,pAuthor){
+	var private_posting_wif = process.env.pass;
+	var parent_author = pAuthor;
+	//var parent_author = '';
+	var parent_permlink = child_permlink;
+	var json_metadata = '';
+	//check author have . then remove that
+	var parent_author_permlink = pAuthor;
+	var dotCheck = ".";
+	if(pAuthor.indexOf(dotCheck) != -1){
+		//replace .
+		var parent_author_permlink = pAuthor.replace(".","dot");
+	}
+
+	const permlink = steem.formatter.commentPermlink(parent_author_permlink, parent_permlink)
+	//const permlink = steem.formatter.commentPermlink('jeaimetu', parent_permlink)
+	var tarotResult = tarot.randomCard();
+	var i = tarotResult.indexOf("##: ");
+	var a = tarotResult.substr(i+4,tarotResult.length-1);
+	a.replace(/\s/g,'');
+	console.log("image name :", a,":");
+	var b = tarotResult.substr(0,i-1);
+	
+	var content = '<table><tr><td>  ';
+	//content += cloudinary.image("00_Fool.jpg", {alt : "Test"})
+	console.log(cloudinary.image(a, {alt : "Test"}));
+	//content += cloudinary.image(a, {alt : "Test"}, {width : 50})
+	content += cloudinary.image(a, {width : 100, height : 200, crop : 'fit' })
+	content += '</td><td><p><strong>안녕하세요. 타로점 결과 입니다. 많이 사용해 주세요.</strong></p><hr><p>';
+	
+	//content += tarot.randomCard();
+	b = b.replace("/","/\n");
+	content += b;
+	content += '</td></tr></table>';
+	
+	steem.broadcast.comment (
+    	private_posting_wif,  // Steemit.com Wallet -> Permissions -> Show Private Key (for Posting)
+    	parent_author,        // empty for new blog post 
+    	parent_permlink,      // main tag for new blog post
+    	'jeaimetu',               // same user the private_posting_key is for
+    	permlink,             // a slug (lowercase 'a'-'z', '0'-'9', and '-', min 1 character, max 255 characters)
+    	'',                // human-readable title
+    	content,                 // body of the post or comment
+    	json_metadata,          // arbitrary metadata
+		function (err, result){
+			if(err)
+				console.log('Failure', err);
+			else
+				console.log('Success');
+		}
+		);
+		
+}
+
+function readData(){
+  MongoClient.connect(url, function(err, db) {
+    var dbo = db.db("heroku_dg3d93pq");
+    var findquery = {steem : false};
+    dbo.collection("board").findOne(findquery, function(err, result){
+      if(result == null){
+      //if result is null, then return -1
+      //do nothing
+      }else{
+      //calling write reply
+      }
+        db.close();
+        });
+        });
+
+}
+
+
+function checkReplies() {
+	steem.api.getContentReplies('jeaimetu', process.env.link, function(err, result){
+		//console.log(err, result);
+		result.forEach((num, idx)=> {
+			//sleep.sleep(21); //do not work what I intended
+			console.log(num.body);
+			if(num.children == 0){
+				var string = "타로";
+				if(num.body.indexOf(string) != -1){
+					console.log('I will make reply for this');
+					console.log('call writingReply for ', idx);
+					writingReply(num.permlink, num.author);
+				}
+			}
+		});
+	});
+}
+
+
+// according to steemit github
+// https://github.com/steemit/steem/blob/master/libraries/protocol/include/steemit/protocol/config.hpp
+// #define STEEMIT_MIN_REPLY_INTERVAL              (fc::seconds(20)) // 20 seconds
+// So I safely add 22secs interval consider delay time.
+setInterval(checkReplies, 22000);
+
+
