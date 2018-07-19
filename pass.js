cefd786f06668bb9b7a59224d5c47fba402e8ed9
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-19 15:57:41 +0900

    Update pass.js

--- pass.js
+++ pass.js
@@ -7,12 +7,13 @@ var url = process.env.MONGODB_URI;
 function passAccount(){
 	MongoClient.connect(url, function(err, db) {
    		var dbo = db.db("heroku_dg3d93pq");
-   		var findquery = { account : id };
+   		
    		dbo.collection("user").find({}).toArray(function(err, res){
       			for(i = 0;i< res.length;i++){
         			bcrypt.hash(pass, process.env.SALT, function(err, hash){
           			var newValue = hash;
           			var myobj = { $set: {pass : newValue}};
+					var findquery = { account : res[i].account };
             			dbo.collection("user").updateOne(findquery, myobj, function(err,result){
             				db.close();
             			});
