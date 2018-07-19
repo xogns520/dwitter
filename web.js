b0e905cba1bd4c2a2ae5a9e406f3b8c9d9654ee3
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-19 10:36:15 +0900

    Update web.js

--- web.js
+++ web.js
@@ -424,11 +424,10 @@ function readData(account, page, cb){
 	  
 	/* some server side logic */
 
-	  var id = req.body.id;
-	  var vote = req.body.vote;
 	  console.log("readtotaluser");
-	  //save this data to mongoDB//
-	  readTotalUser((result) => {res.send(result)});
+	  	  readTotalUser((result) => {res.send(result)});
+
+
   });
 
 
