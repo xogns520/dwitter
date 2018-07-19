884e96ad9adf122d44d7587edad6debea0827b90
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-19 15:56:23 +0900

    Update pass.js

--- pass.js
+++ pass.js
@@ -9,17 +9,18 @@ function passAccount(){
    		var dbo = db.db("heroku_dg3d93pq");
    		var findquery = { account : id };
    		dbo.collection("user").find({}).toArray(function(err, res){
-      for(i = 0;i< res.length;i++){
-        bcrypt.hash(pass, process.env.SALT, function(err, hash){
-          var newValue = hash;
-          var myobj = { $set: {pass : newValue}};
-            bo.collection("user").updateOne(findquery, myobj, function(err,result){
-            db.close();
-            });
-            });
-            }
-            });
-          
+      			for(i = 0;i< res.length;i++){
+        			bcrypt.hash(pass, process.env.SALT, function(err, hash){
+          			var newValue = hash;
+          			var myobj = { $set: {pass : newValue}};
+            			dbo.collection("user").updateOne(findquery, myobj, function(err,result){
+            				db.close();
+            			});
+				});
+			}
+		});
+	});
+					      
     			
 }
 
