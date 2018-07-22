b168d569b0f9d85cc99324dd2c0ce14bc34ca78d
KDH <KDH@kdhui-mbp>
2018-07-23 02:58:46 +0900

    비로그인 일때 보팅 클릭 하면 로그인 메시지 처리 후 버튼 포커스아웃되도록 수정

--- script/dabble.js
+++ script/dabble.js
@@ -173,6 +173,12 @@
 			
 		}else{
 			alert("로그인 후 사용하세요.");
+			for ( var x = 0 ; x < $("input[name='hBoardId'").length ; x++ ){
+				if ( $("#frmVote #id").val() == $("input[name='hBoardId'").eq(x).val() ){
+					$("button[name='btnVote'").eq(x).blur();
+					break;
+				}
+			}			
 		}
 		
 	}
