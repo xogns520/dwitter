fa40259a31ee75321ce533633a530cf8648f5103
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-05 21:33:32 +0900

    Update follower.js

--- follower.js
+++ follower.js
@@ -8,7 +8,7 @@ exports.createFriend = function(account, friend, callback){
   MongoClient.connect(url, function(err, db) {
 	  const dbo = db.db("heroku_dg3d93pq");
 	  const myObj = {account : account, follower : friend};
-	  dbo.collection("board").insertOne(myobj, function(err, res){
+	  dbo.collection("follower").insertOne(myObj, function(err, res){
 		  if (err) throw err;
 		  console.log("1 follower inserted", account, friend);
 		  callback("OK");
@@ -21,7 +21,7 @@ exports.readFriends = function(account,callback){
 	MongoClient.connect(url, function(err, db) {
 	  const dbo = db.db("heroku_dg3d93pq");
 	  const findQuery = {account : account};
-	  dbo.collection("board").find(findQuery).toArray(function(err, res){
+	  dbo.collection("follower").find(findQuery).toArray(function(err, res){
 		  if (err) throw err;
 		  console.log("reading follower", res.length);
 		  //make body
@@ -39,7 +39,7 @@ exports.deleteFriend = function(account, callback){
 	MongoClient.connect(url, function(err, db) {
 		const dbo = db.db("heroku_dg3d93pq");
 		const deleteQuery = {account : account, follower : friend};
-		dbo.collection("board").deleteOne(deleteQuery, function(err, res){
+		dbo.collection("follower").deleteOne(deleteQuery, function(err, res){
 			if (err) throw err;
 			console.log("1 follower deleted", account, friend);
 			callback("OK");
