d595f8cbab1682306ba89a21c58c8a8d0f517c15
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-10 18:03:36 +0900

    Update profile.js

--- profile.js
+++ profile.js
@@ -3,10 +3,10 @@ const ObjectId = require('mongodb').ObjectId;
 const MongoClient = require('mongodb').MongoClient;
 const url = process.env.MONGODB_URI;
 
-exports.setProfilePicture = function(account, url, callback){
+exports.setProfilePicture = function(account, picUrl, callback){
 	MongoClient.connect(url, function(err, db) {
 		const dbo = db.db("heroku_dg3d93pq");
-		const myObj = { $set : { account : account, profile : url}};
+		const myObj = { $set : { account : account, profile : picUrl}};
 		const findQuery = {account : account};
 		dbo.collection("user").updateOne(findQuery, myObj, function(err, res){
 			if(err) throw err;
