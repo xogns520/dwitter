df585bd315dc52d1b537ed0b2047beef16c47606
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-10 11:24:15 +0900

    Update dabble.js

--- script/dabble.js
+++ script/dabble.js
@@ -29,7 +29,7 @@
 			
 			var btnVoteEnable = 'false' == data[x].votingenable ? 'disabled' : '';
 			var profilePath = (data[x].length == 5) ? '"./images/user/' + data[x].profile : '"' + data[x].profile;
-			console.log("profilePath", profilePath);
+	
 				
 			var strHtml	= '<div class="element tile-1 home calc bg-change">'
 						+ '	<table style="width: 100%;">'
@@ -39,7 +39,7 @@
 //						+ '				<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Tooltip on bottom" aria-describedby="tooltip113771">Bottom</button>'
 						+ '			</td>'
 						+ '			<td  style="text-align:right; margin: 0px;" onClick="javascript:gfFollowPopup(' + x + ');">'
-						+ '				<img name="userImage" style="padding: 0px 0px 0px 0px; display: inline; max-height: 40px; max-width: 40px;" src=profilePath + '">'
+						+ '				<img name="userImage" style="padding: 0px 0px 0px 0px; display: inline; max-height: 40px; max-width: 40px;" src=' + profilePath + '">'
 						+ '			</td>'
 						+ '		</tr>'
 						+ '	</table>'
