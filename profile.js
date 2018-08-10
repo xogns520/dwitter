b22be32a01117fda9c09417f70a75af63981c60a
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-10 17:32:40 +0900

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,1 +1,7 @@
+const mongo = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const MongoClient = require('mongodb').MongoClient;
+const url = process.env.MONGODB_URI;
 
+exports.setProfilePicture(account, url, callback){
+}
