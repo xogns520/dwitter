4a1a20f57578b6af9f77db528fed94f63419a65a
KDH <KDH@kdhui-mbp>
2018-08-15 19:51:27 +0900

    더블 쿼테이션 추가;

--- script/dabble.js
+++ script/dabble.js
@@ -38,7 +38,7 @@
 //						+ '				<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Tooltip on bottom" aria-describedby="tooltip113771">Bottom</button>'
 						+ '			</td>'
 						+ '			<td  style="text-align:right; margin: 0px;" onClick="javascript:gfFollowPopup(' + x + ');">'
-						+ '				<img name="userImage" style="padding: 0px 0px 0px 0px; display: inline; max-height: 40px; max-width: 40px;" src=' + profilePath + '">'
+						+ '				<img name="userImage" style="padding: 0px 0px 0px 0px; display: inline; max-height: 40px; max-width: 40px;" src="' + profilePath + '">'
 						+ '			</td>'
 						+ '		</tr>'
 						+ '	</table>'
