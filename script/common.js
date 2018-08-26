3a9496f56f2be245ec06d3cda5ea06df5de22609
KDH <KDH@kdhui-mbp>
2018-08-27 07:01:58 +0900

    harkhark

--- script/common.js
+++ script/common.js
@@ -155,23 +155,6 @@
 
 	}
 	
-	
-	function timeConverter(UNIX_timestamp){
-		  var a = new Date(UNIX_timestamp * 1000);
-		  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-		  //var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-		  var year = a.getFullYear();
-		  var month = a.getMonth()+1;
-		  var date = a.getDate();
-		  var hour = a.getHours();
-		  var min = a.getMinutes();
-		  var sec = a.getSeconds();
-		  //var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-		  var time = year + '-' + month + '-' + date + ' ' + hour + ':' + min + ':' + sec ;
-		  return time;
-	}
-	
-	
 	//http://osvaldas.info/elegant-css-and-jquery-tooltip-responsive-mobile-friendly
 	function gfTooltip(){
 		//tooltip = $( '<div id="tooltip" style="display: inline;"></div>' );
