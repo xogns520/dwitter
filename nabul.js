44bc20dae3e31bccb08adb8c4d1b02867517d363
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-30 00:56:43 +0900

    Update nabul.js

--- nabul.js
+++ nabul.js
@@ -1,1 +1,72 @@
+const mongo = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const MongoClient = require('mongodb').MongoClient;
+const url = process.env.MONGODB_URI;
 
+
+export.readNabul = function(account, page, cb){
+	console.log("in reaData", account, page);
+	MongoClient.connect(url, function(err, db) {
+   		var dbo = db.db("heroku_dg3d93pq");
+		var tod = Date.now();
+		//ToDo : add image path to response
+   		//dbo.collection("board").find({}).sort({date: -1}).toArray(function(err, result){
+		
+		var agr = [	
+      { $match : { 
+			{ $lookup:
+			    { from: 'user',
+			   localField: 'account',
+			   foreignField : 'account',
+			   as : 'userdetails'
+			    }
+			   },
+
+			  {$sort: {"date" : -1}},
+			{$skip : (20 * page) - 20},
+			   {$limit : 20}
+			];
+		
+		dbo.collection("board").aggregate(agr).toArray(function(err, result){
+			    			if (err) throw err;
+			var body = []; // empty array
+			var picUrl;
+			console.log("Readdata size", result.length);
+			//console.log("aggregate data", result);
+			//onsole.log("before for loop");
+			
+			for(i = 0; i < result.length ; i++){				
+				//console.log("in for loop");
+				if(result[i].userdetails[0] == undefined){
+					picUrl = "0.png";
+					//console.log("undefined case");
+				}
+				else{
+					//console.log("define case");
+					var picUrl = result[i].userdetails[0].profile;
+				}
+				
+
+				const votingenable = "true";				
+				body.push({id: result[i]._id, account: result[i].account, data : result[i].data, date : result[i].date,
+					  voting : result[i].voting,  profile : picUrl, votingenable : votingenable });
+				
+			}
+			//generating votingenable flag
+			var findQuery = { account : account };
+			dbo.collection("voting").find(findQuery).toArray(function(err, votingTable){
+				if(err) throw err;
+				console.log("voting table query result",votingTable.length, account, body.length);
+				for(i = 0;i < body.length;i++){
+					for(j = 0;j<votingTable.length;j++)
+						if(body[i].id == votingTable[j].boardId){
+							body[i].votingenable = "false";
+							break;
+						}
+				}
+				cb(body);
+    				db.close();   
+			});
+   		});
+  	}); 
+}
